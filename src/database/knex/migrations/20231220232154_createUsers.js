d9575bd169ed487347726e177b7cca1e08bd6f9a
Pedro Rocha <pedro_r.rocha@hotmail.com>
2023-12-20 20:34:53 -0300

    created knex database

---
+++ src/database/knex/migrations/20231220232154_createUsers.js
@@ -0,0 +1,9 @@
+exports.up = knex => knex.schema.createTable("users", table => {
+    table.increments("id")
+    table.integer("isAdmin")
+    table.text("name")
+    table.text("email")
+    table.text("password")
+})
+
+exports.down = knex => knex.schema.dropTable("users")
