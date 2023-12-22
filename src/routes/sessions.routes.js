c7d09b7195123c59c1010123d10c265d8f95020f
Pedro Rocha <pedro_r.rocha@hotmail.com>
2023-12-22 20:51:07 -0300

    implemented authentication token

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express")
 const sessionsRoutes = Router()
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const SessionsController = require("../controllers/SessionsController")
 const sessionsController = new SessionsController()
 
-sessionsRoutes.post("/", sessionsController.create)
+sessionsRoutes.post("/", ensureAuthenticated, sessionsController.create)
 
 module.exports = sessionsRoutes
