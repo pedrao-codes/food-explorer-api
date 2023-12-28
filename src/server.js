e88502f503fd4963b8d19bfc7a8f9ad611acd4b8
Pedro Rocha <pedro_r.rocha@hotmail.com>
2023-12-28 16:06:35 -0300

    implemented cors

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ require("express-async-errors");
 const express = require("express")
 const routes = require("./routes")
 const AppError = require("./utils/AppError")
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors)
 app.use(express.json())
 app.use(routes)
 
