9a3450b58b16ca03c070b5bb454a21089a5d2753
Pedro Rocha <pedro_r.rocha@hotmail.com>
2023-12-28 18:52:59 -0300

    Revert "implemented cors"
    This reverts commit e88502f503fd4963b8d19bfc7a8f9ad611acd4b8.

--- src/server.js
+++ src/server.js
@@ -3,11 +3,9 @@ require("express-async-errors");
 const express = require("express")
 const routes = require("./routes")
 const AppError = require("./utils/AppError")
-const cors = require("cors")
 
 const app = express()
 
-app.use(cors)
 app.use(express.json())
 app.use(routes)
 
